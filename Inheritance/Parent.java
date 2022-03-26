05d07cba131ed82c0b0f6f71f589eae27a94f53c
Kiggo <97147742+Kiggo@users.noreply.github.com>
2022-03-26 22:08:09 +0900

    Add files via upload

---
+++ Inheritance/Parent.java
@@ -0,0 +1,7 @@
+package Inheritance;
+
+public class Parent {
+	void method1() {System.out.println("Parent method()1...");}
+	void method2() {System.out.println("Parent method()2...");}
+
+}
---
+++ Inheritance/ParentChildTest.java
@@ -0,0 +1,9 @@
+package Inheritance;
+
+public class ParentChildTest {
+	public static void main(String[] args) {
+		Child ch = new Child();
+		ch.method1();
+		ch.method2();
+	}
+}

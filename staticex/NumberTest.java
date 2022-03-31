cf5691278b2073ef0f89401d6e869bc994ef604c
Kiggo <97147742+Kiggo@users.noreply.github.com>
2022-03-31 23:34:04 +0900

    Add files via upload

---
+++ staticex/NumberTest.java
@@ -0,0 +1,24 @@
+package staticex;
+
+public class NumberTest {
+	public static void main(String[] args) {
+		System.out.println("numCount의 값: "+Number.numCounter); //static사용 객체 생성없이 사용가능
+	
+		Number number1 = new Number();
+		number1.increaseNum(); //static이 없어서 객체를 생성하여야한다.
+		System.out.println("numCount의 값: "+Number.getNumCounter());
+		
+		System.out.println("num = "+number1.num());
+		//number1.num();
+		
+		Number number2 = new Number();
+		number2.increaseNum();
+		number2.increaseNum();
+		
+		System.out.println("numCount의 값: "+Number.getNumCounter());
+		System.out.println("numCount의 값: "+number1.getNumCounter());
+		System.out.println("numCount의 값: "+number2.getNumCounter());
+		             // static 멤버에 참조변수로 접근하는 것은 좋은 방법이 아니다.
+		
+	}
+}

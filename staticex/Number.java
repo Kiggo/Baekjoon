85333f7c06300da8c1c0a73215a88328cc5b5636
Kiggo <97147742+Kiggo@users.noreply.github.com>
2022-03-31 23:33:52 +0900

    Create Number.java

---
+++ staticex/Number.java
@@ -0,0 +1,25 @@
+package staticex;
+
+public class Number {
+	private int num;
+	static int numCounter = 10;
+	public static int getNumCounter() {
+		int num1 = 0;
+		num1 = numCounter;
+		//return num; //오류 발생
+		return numCounter;
+	}
+	
+	public Number() {
+		num = numCounter;
+	}
+	
+	public void increaseNum() {
+		numCounter++;
+	}
+	
+	public int num() {
+	//System.out.println(num++);
+		return num++;
+	}
+}

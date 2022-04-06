bd9d5b28b63347368c51f6cb8a82f577cfade0c6
Kiggo <97147742+Kiggo@users.noreply.github.com>
2022-04-06 21:20:11 +0900

    Create Board.java

---
+++ list_ex/example/Board.java
@@ -0,0 +1,14 @@
+package list_ex_ex;
+
+public class Board {
+	private String title;
+	private String content;
+	
+	public Board(String title, String content) {
+		this.title = title;
+		this.content = content;
+	}
+	
+	public String getTitle() {return title;}
+	public String getContent() {return content;}
+}

6ebc7e9c254718b08d962cbad4f7eb6d077e1331
Kiggo <97147742+Kiggo@users.noreply.github.com>
2022-04-09 00:07:31 +0900

    Create JoinEx.java

---
+++ thread_ex/join/JoinEx.java
@@ -0,0 +1,14 @@
+package thread_ex.join;
+
+public class JoinEx {
+	public static void main(String[] args) {
+		SumThread sumThread = new SumThread();
+		sumThread.start();
+		
+		try {
+			sumThread.join(); //sumThread가 종료할 때까지 메인 스레드를 일시 정지시킴
+		} catch (InterruptedException e) {}
+		
+		System.out.println("1~100 합: " +sumThread.getSum());
+	}
+}

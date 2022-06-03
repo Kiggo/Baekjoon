50394ef7669d78469f05b209d77ebbb700ed6cdc
Kiggo <w2nd33@naver.com>
2022-06-03 17:03:54 +0900

    콜라츠 추측

---
+++ programmers/_12943.java
@@ -0,0 +1,25 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class _12943 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long num = sc.nextInt();
+		int answer = 0;
+		while(true) {
+			answer++;
+			if(num%2==0) 
+				num = num /2 ;
+			else 
+				num = num*3+1;
+			if(num==1)
+				break;
+			else if(answer>=500) {
+				answer = -1;
+				break;
+			}
+		}
+		System.out.println(answer);
+	}
+}

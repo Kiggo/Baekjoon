ee95593be5e67ebfc835f676d4365ace67520d6e
Kiggo <w2nd33@naver.com>
2022-06-14 17:01:33 +0900

    Spring Example

---
+++ Spring/spring02/src/main/java/org/zerock/controller/HomeController.java
@@ -0,0 +1,40 @@
+package org.zerock.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {//모델 데이터를 담아서 jsp로 보내는 역할
+													//로컬은 날짜
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate ); //자료저장
+		
+		return "home"; //jsp 
+	}
+	
+}

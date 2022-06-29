b2e95a9af1a4a194351204c23236b97cbb0bdfe2
Kiggo <w2nd33@naver.com>
2022-06-29 16:37:47 +0900

    댓글 페이지 처리

--- Spring/springtest/src/test/java/org/zerock/mapper/ReplyMapperTests.java
+++ Spring/springtest/src/test/java/org/zerock/mapper/ReplyMapperTests.java
@@ -64,10 +64,20 @@ public class ReplyMapperTests {
 		log.info("UPDATE COUNT: " + count);
 	}
 	*/
+	/*
 	@Test
 	public void testList() {
 		Criteria cri = new Criteria();
 		List<ReplyVO> replies = mapper.getListWithPaging(cri, bnoArr[0]);
 		replies.forEach(reply -> log.info(reply));
 	}
+	*/
+	@Test
+	public void testList2() {
+		Criteria cri = new Criteria(2, 10);
+		
+		List<ReplyVO> replies = mapper.getListWithPaging(cri, 2755817L);
+		replies.forEach(reply -> log.info(reply));
+	}
+	
 }

cfa8289ce447132a92ef766e08df246bff7c5b69
Kiggo <w2nd33@naver.com>
2022-07-06 09:15:34 +0900

    reply test

--- Spring/springtest/src/main/java/org/zerock/controller/HomeController.java
+++ Spring/springtest/src/main/java/org/zerock/controller/HomeController.java
@@ -22,22 +22,22 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/main.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		
-		/*
-		 * logger.info("Welcome home! The client locale is {}.", locale);
-		 * 
-		 * Date date = new Date(); DateFormat dateFormat =
-		 * DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		 * 
-		 * String formattedDate = dateFormat.format(date);
-		 * 
-		 * model.addAttribute("serverTime", formattedDate );
-		 */
+	
+		  logger.info("Welcome home! The client locale is {}.", locale);
+		  
+		  Date date = new Date(); DateFormat dateFormat =
+		  DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		  
+		  String formattedDate = dateFormat.format(date);
+		  
+		  model.addAttribute("serverTime", formattedDate );
+		 
 		
 		
-		return "main";
+		return "home";
 	}
 	
 }
